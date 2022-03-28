d6b01aea4a11b22f7b7253a6a16f32bf5075c997
swap123 <misalswapnil77@gmail.com>
2022-03-28 20:30:53 +0530

    Rule-2

--- src/com/regex/src/com/userregistration/UserRegistration.java
+++ src/com/regex/src/com/userregistration/UserRegistration.java
@@ -8,6 +8,8 @@ public class UserRegistration {
     final String NAME = "^[A-Z][a-z]{2,}(\\s)?[A-Z][a-z]{2,}$";
     final String EMAIL = "^[a-zA-Z0-9+_-]+([.][a-zA-Z0-9+_-]+)*@[a-zA-Z0-9]+([.][a-zA-Z0-9+_-]+)+$";
     final String PHONE = "^[0-9]{1,3}[\\s][0-9]{10}$";
+    final String PASSWORD = "^(?=.*[A-Z]).{8,}$";
+
 
     public void firstName(String name) {
 
@@ -24,8 +26,15 @@ public class UserRegistration {
     }
 
     public void phoneNumber(String phone) {
-        Pattern pattern = Pattern.compile(phone);
+        Pattern pattern = Pattern.compile(PHONE);
         Matcher matcher = pattern.matcher(phone);
         System.out.println(matcher.matches());
+    }
+
+    public void password(String password) {
+
+        Pattern pattern = Pattern.compile(PASSWORD);
+        Matcher match = pattern.matcher(password);
+        System.out.println(match.matches());
     }
 }
