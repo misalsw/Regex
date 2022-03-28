8188f5deec4d83e91f0bbdc748eb4cadfc4ded5e
swap123 <misalswapnil77@gmail.com>
2022-03-28 19:58:06 +0530

    Full Name

--- src/com/regex/src/com/userregistration/RegexTest.java
+++ src/com/regex/src/com/userregistration/RegexTest.java
@@ -7,7 +7,7 @@ public class RegexTest {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Please Enter the FirstName: ");
+        System.out.println("Please Enter the FullName: ");
         String name = scan.nextLine();
 
         UserRegistration user = new UserRegistration();
--- src/com/regex/src/com/userregistration/UserRegistration.java
+++ src/com/regex/src/com/userregistration/UserRegistration.java
@@ -5,11 +5,11 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    final String FIRST_NAME = "^[A-Z][a-z]{2,}$";
+    final String NAME = "^[A-Z][a-z]{2,}(\\s)?[A-Z][a-z]{2,}$";
 
     public void firstName(String name) {
 
-        Pattern pattern = Pattern.compile(FIRST_NAME);
+        Pattern pattern = Pattern.compile(NAME);
         Matcher match = pattern.matcher(name);
         System.out.println(match.matches());
     }
